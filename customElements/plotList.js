6e2b02954753f3aed5f2f1b9f519f241d5ae2f8d
BillMills <mills.wj@gmail.com>
2015-11-03 15:45:10 -0800

    basic multi-plot view implemented

---
+++ customElements/plotList.js
@@ -0,0 +1,56 @@
+xtag.register('x-plot-list', {
+    lifecycle:{
+        inserted: function(){
+            //inject template
+            promisePartial('plotList').then(
+                function(template){
+                    this.innerHTML = Mustache.to_html(template, {'topGroups': dataStore.topGroups});
+                }.bind(this)
+            )
+        }
+    },
+
+    methods:{
+        configure: function(){
+            //plug in UI
+            var i, plotSelectorSections, plotToggles;
+         
+            //set up plot selection top row
+            plotSelectorSections = document.getElementsByClassName('topRowSection')
+            for(i=0; i<plotSelectorSections.length; i++){
+                plotSelectorSections[i].onclick = this.selectMenuSection;
+            }
+
+            //set up plot list clicks
+            plotToggles = document.getElementsByClassName('dd-item')
+            for(i=0; i<plotToggles.length; i++){
+                plotToggles[i].onclick = this.selectPlot.bind(this, plotToggles[i].id);
+            }            
+        },
+
+        selectMenuSection: function(){
+            //select the top level menu tab
+            if(dataStore.currentSubrow){
+                document.getElementById(dataStore.currentSubrow).classList.toggle('hidden')
+                document.getElementById('tab' + dataStore.currentSubrow.slice(6)).classList.toggle('active')
+
+            }
+            document.getElementById('subrow' + this.id).classList.toggle('hidden')
+            document.getElementById('tab' + this.id).classList.toggle('active')
+            dataStore.currentSubrow = 'subrow' + this.id;
+        },
+
+        selectPlot: function(plotName, e){
+            //dispatch an event carrying the requested plot name on click of a dropdown element.
+            e.stopPropagation();
+
+            var evt = new CustomEvent('requestPlot', {
+                detail: { 'plotName': plotName },
+                cancelable: true
+            });
+            dataStore.plotNameListeners.map(function(id){
+                document.getElementById(id).dispatchEvent(evt);
+            });
+        }
+    }
+});
