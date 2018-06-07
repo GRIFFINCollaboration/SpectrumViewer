3667d3acfbf2e72601e59f82f81a88bd24b16efd
Adam Garnsworthy <garns@223-s85-v14-wlan.triumf.ca>
2018-06-07 09:26:27 -0700

    Added HPGe BGO spectra to menu

--- scripts/spectrumViewer.js
+++ scripts/spectrumViewer.js
@@ -28,7 +28,7 @@ function setupDataStore(){
     //generate GRIFFFIN BGO detector nomenclature codes
     for(i=1; i<17; i++){
         for(j=0; j<griffinQuads.length; j++){
-            for(k=0; k<5; k++){
+            for(k=1; k<6; k++){
 		grifBGOCodes.push('GRS' + alwaysThisLong(i,2) + griffinQuads[j] + 'N'+ alwaysThisLong(k,2) + 'X');
 	    }
         }
