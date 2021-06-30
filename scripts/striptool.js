255c7aecb293ab015c0740ba880fd5d00125df33
Adam Garnsworthy <garns@Adams-MacBook-Pro.local>
2021-06-30 09:22:48 -0700

    Updated variables of the ratemonitor and striptool due to outdated naming of spectra and variables

--- scripts/striptool.js
+++ scripts/striptool.js
@@ -6,7 +6,7 @@ function setupDataStore(){
     dataStore = {
         ODBhost: 'http://grsmid00.triumf.ca:8081/',                 //MIDAS / ODB host + port
         sourceNamesDir: '/Equipment/Epics/Settings/Names',             //ODB array holding names of things to populate striptool with
-        sourceDataDir: '/Equipment/Epics/Variables/Measured'           //ODB array with data to plot in striptool corresponding to sourceNames
+        sourceDataDir: '/Equipment/Epics/Variables/MSRD'           //ODB array with data to plot in striptool corresponding to sourceNames
     }
 }
 setupDataStore();
