b9cc026ca6448b07d5a316878e9b733787520795
Adam Garnsworthy <garns@Adams-MacBook-Pro.local>
2021-07-05 21:56:47 -0700

    Rate Monitor: Colors

--- scripts/rateMonitor.js
+++ scripts/rateMonitor.js
@@ -436,7 +436,7 @@ function setupDataStore(){
 		axis: 'y2'
 	    },
 	},
-        colors: [ "#D35400", "#002db3" ],
+        colors: [ "#D35400", "#FFFFFF" ],
         axisLabelColor: '#FFFFFF',
         axes: {
             x: {
