e586216f559ec31e4f356dc706d453d0f81e708d
Vinzenz Bildstein <vbildste@uoguelph.ca>
2021-11-27 10:02:11 -0800

    Changed back to grifstore0

--- scripts/striptool.js
+++ scripts/striptool.js
@@ -4,7 +4,7 @@
 
 function setupDataStore(){
     dataStore = {
-        ODBhost: 'http://grifstore1.triumf.ca:8081/',                 //MIDAS / ODB host + port
+        ODBhost: 'http://grifstore0.triumf.ca:8081/',                 //MIDAS / ODB host + port
         sourceNamesDir: '/Equipment/Epics/Settings/Names',             //ODB array holding names of things to populate striptool with
         sourceDataDir: '/Equipment/Epics/Variables/MSRD'           //ODB array with data to plot in striptool corresponding to sourceNames
     }
