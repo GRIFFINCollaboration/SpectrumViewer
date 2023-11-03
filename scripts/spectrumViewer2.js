5d904253105afa957fb1ebe5b305518a87f05a65
Adam Garnsworthy <garnznation@gmail.com>
2023-11-03 10:45:26 -0700

    Add file spectrumViewer2.js
    Add file spectrumViewer2.js which contains a test version of dynamic menu generation based on the list of spectra received from the analyzer server.

---
+++ scripts/spectrumViewer2.js
@@ -0,0 +1,186 @@
+////////////////////////////////////////////
+// main setup
+////////////////////////////////////////////
+
+
+// Obtain the MIDAS server information from the URL
+var urlData = parseQuery();
+var thisSpectrumServer = 'http://'+urlData.backend+'.triumf.ca:'+urlData.port;
+//var thisSpectrumServer = 'http://grifstore0.triumf.ca:9093';
+var SpectrumList;
+var dataStore = {};
+console.log('Server is: '+thisSpectrumServer);
+
+function setupDataStore(callback){
+    console.log('Execute setupDataStore...');
+
+    /*
+    // Test JSON object
+   SpectrumList = {
+                     "Hits_and_Sums" : [
+                                        ["Hits", "HITPATTERN_Energy", "HITPATTERN_Time", "HITPATTERN_Waveform", "HITPATTERN_Pulse_Height", "HITPATTERN_Rate"], 
+                                        ["Sums", "SUM_Singles_GeA_Energy", "SUM_Singles_GeB_Energy", "SUM_SCEPTAR_Energy", "SUM_LaBr3_Energy", "SUM_TAC_Energy"],
+                                        ["Coinc", "COINC_gamma-gamma", "COINC_beta-gamma", "COINC_beta-PACES", "COINC_gamma-PACES", "COINC_beta-Addback"],
+                                        ["DeltaT", "deltaT_gamma-gamma"],
+                     ],
+                     "Griffin" : [
+	                          ["Energy", "GRG01BN00A_Energy", "GRG01GN00A_Energy", "GRG01RN00A_Energy", "GRG01WN00A_Energy", "GRG01BN00B_Energy"],
+	                          ["Time", "GRG01BN00A_Time", "GRG01GN00A_Time", "GRG01RN00A_Time", "GRG01WN00A_Time", "GRG01BN00B_Time"],
+	                 ["Pulse_Height", "GRG01BN00A_Pulse_Height", "GRG01GN00A_Pulse_Height", "GRG01RN00A_Pulse_Height", "GRG01WN00A_Pulse_Height", "GRG01BN00B_Pulse_Height"],
+                     ],
+                  }
+*/
+
+    
+    console.log(SpectrumList);
+
+    //declare the holder for the top level groups
+    var topGroups = [];
+
+    // Sort through the list from the server to find the folders, subfolders and histogram titles
+    // Use this to set up the topGroups, subGroups and items for the menu generation
+    for (i in SpectrumList) 
+    { 
+	thisFolderTitle = i; // this is the topGroup
+
+	// Create a new topGroup for this folder
+	newGroup = {
+                     "name": thisFolderTitle,
+                     "id": thisFolderTitle,
+                     "color": '#367FA9',
+                     "subGroups": []
+	           }
+	
+	for (j in SpectrumList[i]) 
+	{
+	    for (k in SpectrumList[i][j]) 
+	    {
+		y = SpectrumList[i][j][k]
+		if (typeof y === 'string' || y instanceof String){
+		    if(k==0){
+			thisSubfolderTitle = y;   // this is the subGroup
+
+			// Create a new subGroup
+			newSubgroup = {
+                                       "subname": thisSubfolderTitle,
+                                       "id": thisFolderTitle.substring(0,3)+thisSubfolderTitle,
+                                       "items": []
+                                      }
+			// Add this subGroup to the topGroup
+                        newGroup.subGroups.push(newSubgroup);
+		    }else{
+			thisHistoTitle = y;   // this is the items
+
+			// Add this histogram to the items list in this subGroup of the topGroup
+                        newGroup.subGroups[newGroup.subGroups.length-1].items.push(thisHistoTitle);			
+		    }
+		}
+	    }
+	}
+	// Add this new topGroup to the topGroups object
+	topGroups.push(newGroup)
+    }
+    
+    // Create the dataStore object
+  dataStore = {
+        "pageTitle": 'Spectrum Viewer',                             //header title
+        "topGroups": topGroups,                                     //groups in top nav row
+        "waveformSnap": true,                                       //do we want the snap to waveform functionality?
+        "doUpdates": true,                                          //do we want the data update button and loop?
+        "scaling": false,                                           //do we want to expose x-axis rescaling UI?
+        "plots": [],                                                //array of names for default plot cells
+        "spectrumServer": thisSpectrumServer,                       //analyzer url + port number
+        "ODBrequests": [],                                          //array of odb requests to make on refresh
+        "zeroedPlots": {}                                           //initialize empty object for zeroed plots
+  }
+
+    dataStore.cellIndex = dataStore.plots.length;
+
+    callback();
+}
+
+// Get the Spectrum List from the server and then set up the data store. Once that is done then the event listeners can be added.
+GetSpectrumListFromServer(thisSpectrumServer,setupDataStore,setupEventListeners);
+
+
+/////////////////
+// helpers
+/////////////////
+
+function fetchCallback(){
+    //fires after all data has been updated
+
+    var i, 
+        keys = Object.keys(dataStore.viewers);
+
+    for(i=0; i<keys.length; i++){
+        dataStore.viewers[keys[i]].plotData(null, true);
+    }
+}
+
+function GetSpectrumListFromServer(ServerName, callback){
+    console.log('Execute GetSpectrumFromServer...');
+
+    
+    // Get the Spectrum List from the analyser server
+    
+    var errorMessage = 'Error receiving Spectrum List from server, '+ServerName; 
+    var urlString = ServerName+'/?cmd=getSpectrumList';
+    
+    var req = new XMLHttpRequest();
+    req.open('GET', urlString);
+
+    // Once the response is received, convert the text response from the server to JSON Object
+  req.onreadystatechange = () => {
+      if (req.readyState === 4) {
+	 // console.log('Response text is: '+req.response);
+	 // JSONString = req.response.split(")")[0].split("(")[1];
+	  JSONString = req.response;
+	//  console.log('JSON String is: '+JSONString);
+	 SpectrumList = JSON.parse(JSONString);
+	  // The third argument passed to this function is the callback for the next function, so we pass it on here
+          callback(arguments[2]);
+    }
+  };
+
+    // Handle network errors
+    req.onerror = function() {
+        reject(Error(errorMessage));
+    };
+
+    // Make the request
+    req.send();
+
+}
+
+function setupEventListeners(){
+    console.log('Execute setupEventListeners...');
+
+    
+            window.addEventListener('HTMLImportsLoaded', function(e) {
+                dataStore.templates = prepareTemplates(['plotList', 'plotGrid', 'plotControl', 'auxPlotControl', 'auxPlotControlTable', 'fitRow', 'footer']);
+
+                dataStore._plotList = new plotList('analyzerPlots');
+                dataStore._plotGrid = new plotGrid('plottingGrid');
+                dataStore._plotControl = new plotControl('plotCtrl', 'vertical');
+                dataStore._auxCtrl = new auxPlotControl('auxCtrl');
+
+                dataStore._plotList.setup();
+                dataStore._plotGrid.setup();
+                dataStore._plotControl.setup();
+                dataStore._auxCtrl.setup();
+                setupFooter('foot');
+
+                //start with a single plot
+                document.getElementById('plottingGridnewPlotButton').click();
+
+                //start with GRIFFIN menu displayed
+                document.getElementById('Griffin').onclick();
+
+                $(function () {
+                    $('[data-toggle="tooltip"]').tooltip()
+                })
+	    });
+    
+		console.log('Finished setupEventListeners');
+}
