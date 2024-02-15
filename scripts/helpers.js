75df54c90860e1de186d805cdf29c0d40e80eed3
Adam Garnsworthy <garnznation@gmail.com>
2024-02-14 16:47:23 -0800

    dynamic menu generation, added error handling for when the JSON format of the response is not valid.

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -495,8 +495,10 @@ function processSpectrumList(payload,callback){
     catch(err){
 	console.log('Problem with format of the Spectrum list provided by the server for histogram file, '+dataStore.histoFileName);
 	console.log(err);
+	/*
 	const thisTimeout = setTimeout(function() {if(document.getElementById('navbar-content-div').innerHTML){
 	    document.getElementById('navbar-content-div').innerHTML = 'Problem with format of the Spectrum list provided by the server for histogram file, '+dataStore.histoFileName; }, 1000);
+	*/			       
 	return;
     }
     
