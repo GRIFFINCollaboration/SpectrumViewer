9272b18fc98bc0b284434953419885a6c224ae2c
Adam Garnsworthy <garnznation@gmail.com>
2024-05-22 14:00:42 -0700

    analyzerinterface. Added host and expt arguments to URL for ONLINE mode.

--- templates/analyzer-sorting/analyzer-sorting.js
+++ templates/analyzer-sorting/analyzer-sorting.js
@@ -423,7 +423,7 @@ function selectMultipleRows(thisRowID){
 
 function SubmitConnectToOnline(){
     // Issue URL to connect to online
-    var url = dataStore.spectrumServer + '?cmd=addDatafile&filename=ONLINE&host=grsmid00';
+    var url = dataStore.spectrumServer + '?cmd=addDatafile&filename=ONLINE&host=grsmid00&expt=griffin';
         XHR(url, 
             'connecting to ONLINE failed.', 
             function(){return 0},
